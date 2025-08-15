42c0deca0fcc8e944e88439e62d6a15b1537450d
Gabriel Silvestre <gabrielsilvestre1164@gmail.com>
2025-08-14 22:01:07 -0300

    Initial commit

---
+++ src/main/java/com/spring/yonkiblog/YonkiblogApplication.java
@@ -0,0 +1,13 @@
+package com.spring.yonkiblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonkiblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonkiblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/yonkiblog/YonkiblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.yonkiblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonkiblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
